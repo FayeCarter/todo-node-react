565716b8f4f614aef19d17c40aeef149398ddb3b
FayeCarter <fayecarter333@gmail.com>
2020-05-23 09:46:59 +0100

    add show by id route

--- index.js
+++ index.js
@@ -37,6 +37,16 @@ app.get("/todos", async(req,res) => {
 
 //get a todo
 
+app.get("/todos/:id", async(req,res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE id = $1;", [id])
+    res.json(todo.rows[0])
+  } catch (error) {
+    console.log(error.message);
+  }
+})
+
 // update a todo
 
 
